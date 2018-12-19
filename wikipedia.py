6ace3d9eb3a9e1142512eab113e7f7d763941eac
Shawn Zhang <szhang9246@users.noreply.github.com>
2018-12-18 17:35:57 -0800

    Add files via upload

---
+++ wikipedia.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('https://en.wikipedia.org/wiki/Bear')
+soup = BeautifulSoup(page.content, 'html.parser')
+body = soup.find_all('body')
+links = soup.find_all('a')
+bearList = []
+for n in range(len(links)):
+    text = links[n].getText()
+    if len(text) < 20:
+        bearList = bearList + text.split()
+print(bearList)
+
+page2 = requests.get('https://en.wikipedia.org/wiki/Hook')
+soup2 = BeautifulSoup(page2.content, 'html.parser')
+body2 = soup2.find_all('body')
+links2 = soup2.find_all('a')
+hookList = []
+for n in range(len(links2)):
+    text = links2[n].getText()
+    if len(text) < 20:
+        hookList = hookList + text.split()
+print(hookList)
+
+
+
+
